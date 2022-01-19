fc1e2200b8b4149d282b35a1005cb3c8282f2889
hurocup_2 <humanoidgithub@gmail.com>
2022-01-19 19:40:50 +0800

    update py

---
+++ src/strategy/Kidsize_HuroCup/origin_strategy_.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+#coding=utf-8
+import rospy
+import numpy as np
+from Pyhton_API import Sendmessage
+import time
+
+
+
+if __name__ == '__main__':
+    try:
+        send = Sendmessage()
+        while not rospy.is_shutdown():
+            
+
+
+    except rospy.ROSInterruptException:
+        pass
